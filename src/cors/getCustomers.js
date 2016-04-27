1c0af6f2b3377060291c1c81c03a34e574c48740
ismael-soriano <luzaoban@gmail.com>
2016-04-27 13:35:40 +0200

    get customers from Web API

---
+++ src/cors/getCustomers.js
@@ -0,0 +1,20 @@
+(function (namespace) {
+
+        $.get("http://localhost:4239/api/customer")
+        .then(function (data) {
+            var target = $('body'),
+                source = $('table>tbody');
+
+            data.forEach(function (item) {
+                var tableRow = $('<tr/>');
+                
+                tableRow.append($('<td/>').text(item.Id));
+                tableRow.append($('<td/>').text(item.Name));
+                tableRow.append($('<td/>').text(item.Phone));
+                source.append(tableRow);
+            });
+
+            target.append(source);
+            //alert("Load was performed.");
+        });       
+}())
